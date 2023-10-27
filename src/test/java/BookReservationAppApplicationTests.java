d206a414b8004ea94e8bc6455d3a70efb78710d7
Vladislav <vlad.iliuk@gmail.com>
2023-10-27 16:50:15 +0300

    first commit

---
+++ src/test/java/BookReservationAppApplicationTests.java
@@ -0,0 +1,45 @@
+
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+import org.example.UserData;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.responseSpecification;
+
+class BookReservationAppApplicationTests {
+	private  final static String URL = "http://localhost:8080/";
+
+	@Test
+	 void contextLoads() {
+	}
+	@Test
+	void checkIdTest() {
+		List<UserData> users = given()
+		.when()
+		.contentType(ContentType.JSON)
+		.get(URL+"categories")
+		.then().log().all()
+		.extract().body().jsonPath().getList("data", UserData.class);
+		//tikrinam ar yra vienodi user
+//		List<String> ids = users.stream().map(x->x.getId().toString()).collect(Collectors.toList());
+//		for (int i = 0; i < ids.size(); i++) {
+//            Assert.isTrue(ids.get(i).contains(ids.get(i)));
+			//validacija patikrint
+
+//        }
+	}
+public void SuccesRegTest() {
+		Integer id;
+		String token;
+		Register user = new Register("Tomas", "1234567889");
+		SuccessReg seccessReg = given()
+				.body(user)
+				.when()
+				.post("categories/")
+				.then().log().all()
+				.extract().as(SuccessReg.class);
+}
+}
---
+++ src/test/java/Register.java
@@ -0,0 +1,9 @@
+public class Register {
+    private String login;
+    private String password;
+
+    public Register(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
