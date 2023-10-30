8cdfad3b397fce778cd2006b046425eacd9b2ed3
Vladislav <vlad.iliuk@gmail.com>
2023-10-30 09:52:33 +0200

    finish pudate PageClicTest, pom

--- src/test/java/PageClicTest.java
+++ src/test/java/PageClicTest.java
@@ -1,9 +1,9 @@
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.*;
 
-public class pageClicTest extends BaseSettingsTest {
+public class PageClicTest extends BaseSettingsTest{
     @Test
-    void clicTest() {
+     void testClick() {
         open("http://localhost:3000/");
         $x("//a/img").click();
         $x("//ul/li/a[@class='nav-link'][1]").click();
@@ -13,7 +13,5 @@ public class pageClicTest extends BaseSettingsTest {
         $x("//div[@id='navbarSupportedContent']/ul//a[@href='/about']").click();
         $x("//div[@id='navbarSupportedContent']//input[@class='form-control me-2']").
                 setValue("^(?!.*(.)\\1)[A-Z][a-z]{4,49}$").pressEnter();
-
     }
-
 }
